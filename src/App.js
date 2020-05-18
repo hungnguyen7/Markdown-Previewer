ed07b0eebf18c2210a681ae037fa5fb6f6af7427
hungnguyen7 <hungnguyenst.99@gmail.com>
2020-05-18 10:28:16 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import marked from 'marked';
 
 const renderer = new marked.Renderer();
+marked.setOptions({
+  breaks: true,
+});
+
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      markdown: '',
+      markdown: placeholder,
       editorMaximized: false, //cua so editor se phong to khi co gia tri bang true
       previewMaximized: false
     }
@@ -41,11 +45,11 @@ class App extends React.Component{
     return(
       <div>
         <div className={classes[0]}>
-          <Toolbar icon={classes[2]} onClick={this.handleEditorMaximize}/>
+          <Toolbar icon={classes[2]} onClick={this.handleEditorMaximize} text="Editor"/>
           <Editor markdown={this.state.markdown} onChange={this.handleChange} />
         </div>
         <div className={classes[1]}>
-          <Toolbar icon={classes[2]} onClick={this.handlePreviewMaximize}/>
+          <Toolbar icon={classes[2]} onClick={this.handlePreviewMaximize} text="Previewer"/>
           <Preview markdown={this.state.markdown}/>
         </div>
       </div>
@@ -56,7 +60,7 @@ class App extends React.Component{
 const Toolbar=(props)=>{
   return (
     <div className="toolbar">
-      <i title="no-stack-dub-sack" className="fa fa-free-code-camp"/>
+      <i title="no-stack-dub-sack" className="fa fa-optin-monster"/>
       {props.text}
       <i onClick={props.onClick} className={props.icon}/>
     </div>
@@ -75,4 +79,33 @@ const Preview=(props)=>{
     </div>
   )
 }
+
+const placeholder = `# Hello my name is Hung Nguyen
+## Welcome to my Markdown Previewer
+
+[My Github account](https://github.com/hungnguyen7)
+
+I'm a \`coder\`
+
+
+\`\`\`
+// this is multi-line code:
+
+const markdown=(firstLine, lastLine)=>{
+  console.log("This is multi-line code");
+}
+\`\`\`
+
+1. First ordered list item
+2. Another item
+
+>Tri nhân giả tri, tự tri khả trí. Thắng nhân giả lực, tự thắng giả cường.
+
+**A JavaScript library for building user interfaces
+Get Started
+Take the Tutorial** 
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+
+`;
 export default App;
